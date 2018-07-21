3ac5f46b46270f056070650306f0704d52e56755
Jake Huang <jdh10791@gmail.com>
2018-07-21 12:19:42 -0600

    renamed package

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='py-casaxps',
       version='1.0',
       description='Plotting tools for CasaXPS data',
-      url='http://github.com/jdh10791/py-casaxps',
+      url='https://github.com/jdh10791/py-casaxps',
       author='Jake Huang',
       #author_email='',
-      license='MIT',
+      #license='MIT',
       packages=['py-casaxps']
       #zip_safe=False)
 	  )
