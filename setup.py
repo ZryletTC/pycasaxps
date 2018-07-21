24a03daa752f496e63126e3c5461c7ebb97b9689
Jake Huang <jdh10791@gmail.com>
2018-07-21 12:48:47 -0600

    Added example code and data

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='py-casaxps',
+setup(name='pycasaxps',
       version='1.0',
       description='Plotting tools for CasaXPS data',
       url='https://github.com/jdh10791/py-casaxps',
       author='Jake Huang',
       #author_email='',
       #license='MIT',
-      packages=['py-casaxps']
+      packages=['pycasaxps']
       #zip_safe=False)
 	  )
