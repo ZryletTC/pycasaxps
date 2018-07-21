740ceed776c813c8f984f9e7834e41e7245ddb1e
Jake Huang <jdh10791@gmail.com>
2018-07-21 11:41:45 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='py-casaxps',
+      version='1.0',
+      description='Plotting tools for CasaXPS data',
+      url='http://github.com/jdh10791/py-casaxps',
+      author='Jake Huang',
+      #author_email='',
+      license='MIT',
+      packages=['py-casaxps']
+      #zip_safe=False)
+	  )
