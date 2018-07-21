e73d0531a703e4306866856398706d91c0c8cb6c
Jake Huang <jdh10791@gmail.com>
2018-07-21 13:47:26 -0600

    Added license

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(name='pycasaxps',
       description='Plotting tools for CasaXPS data',
       url='https://github.com/jdh10791/py-casaxps',
       author='Jake Huang',
-      #author_email='',
-      #license='MIT',
-      packages=['pycasaxps']
-      #zip_safe=False)
-	  )
+      author_email='jdh10791@gmail.com',
+      license='MIT',
+      packages=['pycasaxps'])
